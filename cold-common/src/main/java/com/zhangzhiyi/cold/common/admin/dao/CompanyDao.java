68f6fed22fa23ed839fd8b804eadf08bc0667ac0
zhangzhiyi2162410103 <2606616057@xiaomi.com>
2025-04-28 21:45:19 +0800

    企业信息表实体类

---
+++ cold-common/src/main/java/com/zhangzhiyi/cold/common/admin/dao/CompanyDao.java
@@ -0,0 +1,8 @@
+package com.zhangzhiyi.cold.common.admin.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CompanyDao extends BaseMapper<> {
+}
