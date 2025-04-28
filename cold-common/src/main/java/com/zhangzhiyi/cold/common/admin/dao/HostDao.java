3639371b4312e17fbb76aa06fe2f25e9b33e7e8f
zhangzhiyi2162410103 <2606616057@xiaomi.com>
2025-04-28 21:50:49 +0800

    主机表接口

--- cold-common/src/main/java/com/zhangzhiyi/cold/common/admin/dao/HostDao.java
+++ cold-common/src/main/java/com/zhangzhiyi/cold/common/admin/dao/HostDao.java
@@ -1,10 +1,12 @@
 package com.zhangzhiyi.cold.common.admin.dao;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhangzhiyi.cold.common.admin.entity.HostEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 主机表
  */
 @Mapper
-public interface HostDao {
+public interface HostDao extends BaseMapper<HostEntity> {
 }
